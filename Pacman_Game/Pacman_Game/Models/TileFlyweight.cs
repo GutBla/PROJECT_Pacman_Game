f68d9d3ddcfa0ab3e9e3f3354bab4a3c08c164bb
Andrea Gutiérrez Blanco <Andrea.Gutierrez0724@jala.university>
2026-04-02 00:46:13 -0400

    refactor: Implement TileFlyweight and TileFlyweightFactory for tile management

--- Pacman_Game/Pacman_Game/Models/TileFlyweight.cs
+++ Pacman_Game/Pacman_Game/Models/TileFlyweight.cs
@@ -4,34 +4,13 @@ namespace Pacman_Game.Models
 {
     public class TileFlyweight
     {
-        public string TextureKey { get; } 
+        public string TextureKey { get; }
         public bool IsBlocking { get; }
 
         public TileFlyweight(string textureKey, bool isBlocking)
         {
             TextureKey = textureKey;
             IsBlocking = isBlocking;
-        }
-    }
-
-    public static class TileFlyweightFactory
-    {
-        private static Dictionary<string, TileFlyweight> _tiles = new();
-
-        public static TileFlyweight GetTile(string textureKey, bool isBlocking) // Retorna tile único
-        {
-            string key = $"{textureKey}_{isBlocking}";
-            if (!_tiles.ContainsKey(key))
-            {
-                _tiles[key] = new TileFlyweight(textureKey, isBlocking);
-            }
-            return _tiles[key];
-        }
-
-        public static TileFlyweight GetTileFromMapData(string textureKey, int gameMapValue) // Determina si bloquea según valor del mapa
-        {
-            bool isBlocking = gameMapValue == 1;
-            return GetTile(textureKey, isBlocking);
         }
     }
 }
